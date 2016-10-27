c75f7045ca5b8296e7022b09c97fe506112090a4
Arka Sadhu <ark.sadhu2904@gmail.com>
2016-10-27 13:50:24 +0530

    Initialize Assignment 2_3

---
+++ Assignment2_3/Assignment2_3/animate_class.hpp
@@ -0,0 +1,8 @@
+#ifndef _ANIMATE_CLASS_HPP_
+#define _ANIMATE_CLASS_HPP_
+
+//Define all classes like the color class, adding appropriate methods and data members. 
+//Implementation of the methods go into the corresponding cpp file
+
+
+#endif
